c8627211aae514e76d95980fd543af45ad61da89
Roman Yaremenko <romanyaremenko95@gmail.com>
2021-11-21 01:33:00 +0200

    fix

--- dist/app.js
+++ dist/app.js
@@ -10,17 +10,5 @@
   window.addEventListener("load", function(event) {
     HomePage();
   });
-  var logoNeon = document.querySelector(".logo__neon");
-  var delay = 1500;
-  var timerId = setTimeout(function request() {
-    if (logoNeon.classList.contains("neon")) {
-      delay = 3e3;
-      logoNeon.classList.remove("neon");
-    } else {
-      delay = 1500;
-      logoNeon.classList.add("neon");
-    }
-    timerId = setTimeout(request, delay);
-  }, delay);
 })();
 //# sourceMappingURL=app.js.map
